5c9a3d75f42a0721a512fee306766bd687c060c4
dogasuzer <doga.suzer@hotmail.com>
2023-03-30 23:40:14 +0300

    first version of profile page

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,10 +1,41 @@
 import { View, Text } from "react-native";
 import React from "react";
+import BoldText from "../components/ui/customTexts/BoldText";
+import COLORS from "../components/ui/colors";
+import CustomButton from "../components/ui/CustomButton";
 
 const Profile = () => {
+  const name = "dummyname";
+  const email = "dummyemail";
   return (
-    <View>
-      <Text>Profile</Text>
+    <View
+      style={{ justifyContent: "center", alignItems: "center", height: "100%" }}
+    >
+      <BoldText
+        style={{
+          fontSize: 90,
+          color: "white",
+          backgroundColor: COLORS.secondary200,
+          width: 120,
+          height: 120,
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 60,
+          marginBottom: 20,
+        }}
+      >
+        D
+      </BoldText>
+      <BoldText style={{ fontSize: 30 }}>{name}</BoldText>
+      <BoldText style={{ color: COLORS.gray100, fontSize: 17, marginTop: 10 }}>
+        {email}{" "}
+      </BoldText>
+      <CustomButton
+        style={{ backgroundColor: COLORS.secondary, width: 150, marginTop: 30 }}
+        onPress={{}}
+        text={"Logout"}
+        textColor={COLORS.gray500}
+      />
     </View>
   );
 };
