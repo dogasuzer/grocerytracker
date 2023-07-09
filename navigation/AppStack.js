4cb20b072c23eb8009420dc07843599c14b3f323
dogasuzer <doga.suzer@hotmail.com>
2023-07-09 20:40:36 +0300

    adding tabBar icons

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -1,11 +1,12 @@
-import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import AllGroceries from "../screens/groceries/AllGroceries";
-import Lists from "../screens/Lists";
-import Profile from "../screens/Profile";
-import SingleList from "../screens/SingleList";
-import Groceries from "../screens/groceries/Groceries";
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AllGroceries from '../screens/groceries/AllGroceries';
+import { Ionicons } from '@expo/vector-icons';
+import Lists from '../screens/Lists';
+import Profile from '../screens/Profile';
+import SingleList from '../screens/SingleList';
+import Groceries from '../screens/groceries/Groceries';
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 function ListScreeens() {
@@ -19,13 +20,34 @@ function ListScreeens() {
 const AppStack = () => {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Groceries" component={Groceries} />
+      <Tab.Screen
+        name="Groceries"
+        component={Groceries}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="nutrition" size={20} color={color} />
+          )
+        }}
+      />
       <Tab.Screen
         name="ListScreeens"
         component={ListScreeens}
-        options={{ tabBarLabel: "Lists" }}
+        options={{
+          tabBarLabel: 'Lists',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="menu" size={18} color={color} />
+          )
+        }}
       />
-      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen
+        name="Profile"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person" size={20} color={color} />
+          )
+        }}
+        component={Profile}
+      />
     </Tab.Navigator>
   );
 };
